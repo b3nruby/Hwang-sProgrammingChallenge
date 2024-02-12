10697fc615790ff0f113d3d3f1e73a8ec39510d9
b3nruby <benrubyramirez@gmail.com>
2024-02-12 12:05:44 -0800

    removing previous file errors

--- receiver.c
+++ receiver.c
@@ -1,89 +1,74 @@
 #include "winHead.h"
 
-int main() {
-
-#if defined(_WIN32)
+int main(int args, char *argv[]) {
     WSADATA d;
     if (WSAStartup(MAKEWORD(2, 2), &d)) {
         fprintf(stderr, "Failed to initialize.\n");
         return 1;
     }
-#endif
 
-    printf("Configuring local address...\n");
+    if (args < 3) {
+        fprintf(stderr, "usage: tcp_client hostname port\n");
+        return 1;
+    }
+
+    printf("Configuring remote address to second computer...\n");
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    struct addrinfo *bind_address;
-    getaddrinfo(0, "8081", &hints, &bind_address);
-
-
+    struct addrinfo *peer_address;
+    // get IP from command line
+    if (getaddrinfo(argv[1] , "8080", &hints, &peer_address)) {
+        fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+    printf("Remote address is: ");
+    char address_buffer[100];
+    char service_buffer[100];
+    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,
+                address_buffer, sizeof(address_buffer),
+                service_buffer, sizeof(service_buffer),
+                NI_NUMERICHOST | NI_NUMERICSERV);
+    printf("%s %s\n", address_buffer, service_buffer);
     printf("Creating socket...\n");
-    SOCKET socket_listen;
-    socket_listen = socket(bind_address->ai_family,
-                           bind_address->ai_socktype, bind_address->ai_protocol);
-    if (!ISVALIDSOCKET(socket_listen)) {
+    SOCKET socket_peer;
+    socket_peer = socket(peer_address->ai_family,
+                         peer_address->ai_socktype, peer_address->ai_protocol);
+    if (!ISVALIDSOCKET(socket_peer)) {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
+
     }
 
+    //messages sending to the second computer
+    const char *message = " Hello how's it going.";
+    printf("Sending: %s\n", message, argv[2]);
 
-    printf("Binding socket to local address...\n");
-    if (bind(socket_listen,
-             bind_address->ai_addr, bind_address->ai_addrlen)) {
-        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
-    }
-    freeaddrinfo(bind_address);
+    //concatnates message and argv[2] into new message
+    const char *newMessage = strcat(argv[2],message);
+    int bytes_sent = sendto(socket_peer,newMessage, strlen(newMessage),0,peer_address->ai_addr, peer_address->ai_addrlen);
+    printf("Sent %d bytes.\n", bytes_sent);
+
+    //receive checks for bytes if empty it continues
+    char read [1024];
+    int bytes_check = 1 ;
+    do {
+        int bytes_received = recvfrom(socket_peer,
+                                      read, 1024,
+                                      0,
+                                      (struct sockaddr *) &peer_address, &peer_address->ai_addrlen);
+        printf("%d bytes received. \n Message received:\n",bytes_received,read);
+        bytes_check = bytes_received;
+        Sleep(5);
+    } while(bytes_check > 0);
+    ;
 
 
-    fd_set master;
-    FD_ZERO(&master);
-    FD_SET(socket_listen, &master);
-    SOCKET max_socket = socket_listen;
+    freeaddrinfo(peer_address);
+    CLOSESOCKET(socket_peer);
 
-    printf("Waiting for connections...\n");
-
-
-    while(1) {
-        fd_set reads;
-        reads = master;
-        if (select(max_socket+1, &reads, 0, 0, 0) < 0) {
-            fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
-            return 1;
-        }
-
-        if (FD_ISSET(socket_listen, &reads)) {
-            struct sockaddr_storage client_address;
-            socklen_t client_len = sizeof(client_address);
-
-            char read[1024];
-            int bytes_received = recvfrom(socket_listen, read, 1024, 0,
-                                          (struct sockaddr *)&client_address, &client_len);
-            //print the message
-
-            if (bytes_received < 1) {
-                fprintf(stderr, "connection closed. (%d)\n",
-                        GETSOCKETERRNO());
-                break;
-            }else {
-                const char *ack = " Acknowledged \n";
-                int tBytes = sendto(socket_listen, ack, strlen(ack), 0,
-                                    (struct sockaddr *) &client_address, client_len);
-                printf("Message sent: %s \n", ack);
-                printf("Bytes %d sent\n", tBytes);
-
-            }
-        } //if FD_ISSET
-    } //while(1)
-
-    printf("Closing listening socket...\n");
-    CLOSESOCKET(socket_listen);
     WSACleanup();
-    printf("Finished.\n");
 
+    printf("Finished.\n");
     return 0;
 }
